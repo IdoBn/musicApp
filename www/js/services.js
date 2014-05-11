2ab237afc1c71b782919688f929ee39784bfe00d
Ido Ben-Natan <ido.bennatan@gmail.com>
2014-05-11 14:39:50 +0300

    code refactoring

--- www/js/services.js
+++ www/js/services.js
@@ -1,44 +1,44 @@
 var services = angular.module('starter.services', []);
 
 services.factory('Party', function($http) {
-    function load(path) {
-        return $http.get('http://music-hasalon.herokuapp.com/' + path + '.json');
-    }
+  function load(path) {
+    return $http.get('http://music-hasalon.herokuapp.com/' + path + '.json');
+  }
 
-    return {
-        getParties: function() {
-            return load('party');
-        },
+  return {
+    getParties: function() {
+      return load('party');
+    },
 
-        getParty: function(id) {
-            return load('party/' + id);
-        },
+    getParty: function(id) {
+      return load('party/' + id);
+    },
 
-        search: function(id, songpull) {
-            return $http({
-                    url: 'http://music-hasalon.herokuapp.com/party/' + id + '/search.json',
-                    method: 'GET',
-                    params: {
-                        songpull: songpull
-                    }
-                });
-        },
-
-        createRequest: function(song, partyId) {
-            return $http({
-                url: 'http://music-hasalon.herokuapp.com/requests',
-                method: 'POST',
-                data: {
-                    request: {
-                        title: song.title,
-                        author: song.author.name,
-                        url: song.player_url,
-                        party_id: partyId,
-                        thumbnail: song.thumbnails[0].url
-                    }
-                }
-            });
+    search: function(id, songpull) {
+      return $http({
+        url: 'http://music-hasalon.herokuapp.com/party/' + id + '/search.json',
+        method: 'GET',
+        params: {
+          songpull: songpull
         }
-        
+      });
+    },
+
+    createRequest: function(song, partyId) {
+      return $http({
+        url: 'http://music-hasalon.herokuapp.com/requests',
+        method: 'POST',
+        data: {
+          request: {
+            title: song.title,
+            author: song.author.name,
+            url: song.player_url,
+            party_id: partyId,
+            thumbnail: song.thumbnails[0].url
+          }
+        }
+      });
     }
+    
+  }
 });
